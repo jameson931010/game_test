8e58dfd4d0152146ba6e68a3191c6df0a8c6317d
jameson931010 <jameson931010@gmail.com>
2024-02-15 17:48:30 +0800

    [test] modify test

--- Test.cs
+++ Test.cs
@@ -7,12 +7,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        123456;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        456456;
     }
 }
