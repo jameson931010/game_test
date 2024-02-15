dff7dde18ea784b296c4042d2010243e2c87e588
I3IRI3IR <brian930922@gmail.com>
2024-02-15 17:58:51 +0800

    find_the_flag

--- game_test/Assets/palette/NewBehaviourScript.cs
+++ game_test/Assets/palette/NewBehaviourScript.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        123456;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        456456;
-        //flag{yeeyee}
+        
     }
 }
